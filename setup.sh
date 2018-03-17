603fa50a380765a3d99eb98973d95f1b106efcf8
well <2236zml@gmail.com>
2018-03-17 17:10:58 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 apt-get update
 apt -y install git
+git config --global user.email "2236zml@gmail.com"
+git config --global user.name "well"
 git clone https://github.com/2236zml/docker.git 
 cd docker
 ./run.sh
